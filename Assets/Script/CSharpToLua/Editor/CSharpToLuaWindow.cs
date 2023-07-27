3f3affc2dc7ca6d1563ba0ce730d57c61c734199
Ctyring <75520657+Ctyring@users.noreply.github.com>
2023-07-27 18:39:03 +0800

    添加主要框架

---
+++ Assets/Script/CSharpToLua/Editor/CSharpToLuaWindow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Script.CSharpToLua.Editor {
+    public class CSharpToLuaWindow : EditorWindow {
+        [MenuItem("测试/生成ast")]
+        public static void Test()
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+}
