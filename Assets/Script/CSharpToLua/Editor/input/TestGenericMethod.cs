9c16bb15bfe11f6de879927314a91c90251b1c0f
Ctyring <75520657+Ctyring@users.noreply.github.com>
2023-08-05 22:50:03 +0800

    完成泛型方法

---
+++ Assets/Script/CSharpToLua/Editor/input/TestGenericMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace Script.CSharpToLua.Editor.input {
+    public class TestGenericMethod {
+        public T Test<T>(T t) {
+            return t;
+        }
+
+        public void TestDo() {
+            Test<int>(5);
+        }
+    }
+}
